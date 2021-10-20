9a3674dcc8794d2d1d29fcd3085c0dfed1d23b8d
bvalette <bvalette@student.42.fr>
2021-10-20 08:02:59 +0200

    🐛 fix(backend): recreated buggy project template

--- back_end/src/app.module.ts
+++ back_end/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostsController } from './posts/posts.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, PostsController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
