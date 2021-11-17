10010de6054bc8f6601c44a23cd97a89660634d3
bvalette <bvalette@student.42.fr>
2021-11-17 13:58:18 +0100

    👓 chore(back/users): cleanup file user controller

--- back_end/src/module_users/users.controller.ts
+++ back_end/src/module_users/users.controller.ts
@@ -1,7 +1,20 @@
 import {
-  Body, Controller, Delete, Get, Patch, Post, Session, UseGuards
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  Session,
+  UseGuards,
 } from '@nestjs/common';
-import { ApiBody, ApiCookieAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiCookieAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AuthGuard } from '../guards/auth.guard';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { AddFriendDto } from './dtos/add-friend.dto';
@@ -9,10 +22,15 @@ import { UpdateUserDto } from './dtos/update-users.dto';
 import { UserDto } from './dtos/user.dto';
 import { User } from './entities/users.entity';
 import { Serialize } from './interceptors/serialize.interceptor';
-import { RelationsService, RelationType } from './service_friends/relations.service';
+import {
+  RelationsService,
+  RelationType,
+} from './service_friends/relations.service';
 import { UsersService } from './service_users/users.service';
 
 @ApiTags('Users')
+@ApiCookieAuth()
+@UseGuards(AuthGuard)
 @Serialize(UserDto)
 @Controller('users')
 export class UsersController {
@@ -21,142 +39,112 @@ export class UsersController {
     private friendsService: RelationsService,
   ) {}
 
-    @Get('/')
-    @UseGuards(AuthGuard)
-    @ApiOperation({
-      summary: 'Get every users in the database'
-    })
-    @ApiCookieAuth()
-    async getAllUsers() {
-      return await this.usersService.getAllUsers();
-    }
+  @Get('/')
+  @ApiOperation({
+    summary: 'Get every users in the database',
+  })
+  async getAllUsers() {
+    return await this.usersService.getAllUsers();
+  }
 
-    @Get('/me')
-    @UseGuards(AuthGuard)
-    @ApiOperation({
-      summary: 'Get infos of the currently logged user'
-    })
-    @ApiCookieAuth()
-    @ApiResponse({ type: UserDto })
-    whoAmI(@CurrentUser() user: User) {
-      return user;
-    }
+  /*****************************************************************************
+   *    CURRENTLY LOGGED USER
+   *****************************************************************************/
 
-    @Patch('/me')
-    @UseGuards(AuthGuard)
-    @ApiOperation({
-      summary: 'Update infos of the currently logged user'
-    })
-    @ApiCookieAuth()
-    @ApiBody({ type: UpdateUserDto })
-    @ApiResponse({ type: UserDto })
-    async update(@Body() body: Partial<UpdateUserDto>, @Session() session: any) {
-      return this.usersService.update(session.userId.id, body);
-    }
+  @Get('/me')
+  @ApiOperation({
+    summary: 'Get infos of the currently logged user',
+  })
+  @ApiResponse({ type: UserDto })
+  whoAmI(@CurrentUser() user: User) {
+    return user;
+  }
 
-    @Get('/friends')
-    @UseGuards(AuthGuard)
-    @ApiOperation({
-      summary: 'Get list of friends of the currently logger user'
-    })
-    @ApiCookieAuth()
-    async getAllFriends(@Session() session) {
-      return await this.friendsService.getAllRelations(session.userId.id, RelationType.Friend );
-    }
+  @Patch('/me')
+  @ApiOperation({
+    summary: 'Update infos of the currently logged user',
+  })
+  @ApiBody({ type: UpdateUserDto })
+  @ApiResponse({ type: UserDto })
+  async update(@Body() body: Partial<UpdateUserDto>, @Session() session: any) {
+    return this.usersService.update(session.userId.id, body);
+  }
 
-    @Post('/friends')
-    @UseGuards(AuthGuard)
-    @ApiOperation({
-      summary: 'Add one friend to the currently logger user'
-    })
-    @ApiCookieAuth()
-    async addFriend(@Body() friendId: AddFriendDto, @Session() session) {
-      return await this.friendsService.addRelation(session.userId.id, friendId.id, RelationType.Friend);
-    }
+  /*****************************************************************************
+   *    FRIENDS
+   *****************************************************************************/
 
-    @Delete('/friends')
-    @UseGuards(AuthGuard)
-    @ApiOperation({
-      summary: 'Remove one friend to the currently logger user'
-    })
-    @ApiCookieAuth()
-    async removeFriend(@Body() friendId: AddFriendDto, @Session() session) {
-      return await this.friendsService.removeRelation(session.userId.id, friendId.id, RelationType.Friend);
-    }
+  @Get('/friends')
+  @ApiOperation({
+    summary: 'Get list of friends of the currently logger user',
+  })
+  async getAllFriends(@Session() session) {
+    return await this.friendsService.getAllRelations(
+      session.userId.id,
+      RelationType.Friend,
+    );
+  }
 
-    @Get('/block')
-    @UseGuards(AuthGuard)
-    @ApiOperation({
-      summary: 'Get list of blocked accounts of the currently logger user'
-    })
-    @ApiCookieAuth()
-    async getAllBlocks(@Session() session) {
-      return await this.friendsService.getAllRelations(session.userId.id, RelationType.Block);
-    }
+  @Post('/friends')
+  @ApiOperation({
+    summary: 'Add one friend to the currently logger user',
+  })
+  async addFriend(@Body() friendId: AddFriendDto, @Session() session) {
+    return await this.friendsService.addRelation(
+      session.userId.id,
+      friendId.id,
+      RelationType.Friend,
+    );
+  }
 
-    @Post('/block')
-    @UseGuards(AuthGuard)
-    @ApiOperation({
-      summary: 'Add one blocked account to the currently logger user'
-    })
-    @ApiCookieAuth()
-    async addBlock(@Body() friendId: AddFriendDto, @Session() session) {
-      return await this.friendsService.addRelation(session.userId.id, friendId.id, RelationType.Block);
-    }
+  @Delete('/friends')
+  @ApiOperation({
+    summary: 'Remove one friend to the currently logger user',
+  })
+  async removeFriend(@Body() friendId: AddFriendDto, @Session() session) {
+    return await this.friendsService.removeRelation(
+      session.userId.id,
+      friendId.id,
+      RelationType.Friend,
+    );
+  }
 
-    @Delete('/block')
-    @UseGuards(AuthGuard)
-    @ApiOperation({
-      summary: 'Remove one blocked account to the currently logger user'
-    })
-    @ApiCookieAuth()
-    async removeBlock(@Body() friendId: AddFriendDto, @Session() session) {
-      return await this.friendsService.removeRelation(session.userId.id, friendId.id, RelationType.Block);
-    }
+  /*****************************************************************************
+   *    BLOCKED ACCOUNTS
+   *****************************************************************************/
 
+  @Get('/block')
+  @ApiOperation({
+    summary: 'Get list of blocked accounts of the currently logger user',
+  })
+  async getAllBlocks(@Session() session) {
+    return await this.friendsService.getAllRelations(
+      session.userId.id,
+      RelationType.Block,
+    );
+  }
 
+  @Post('/block')
+  @ApiOperation({
+    summary: 'Add one blocked account to the currently logger user',
+  })
+  async addBlock(@Body() friendId: AddFriendDto, @Session() session) {
+    return await this.friendsService.addRelation(
+      session.userId.id,
+      friendId.id,
+      RelationType.Block,
+    );
+  }
 
-
-
-
-
-
-  // @ApiResponse({ type: UserDto })
-  // @Post('/signup')
-  // async createUser(@Body() body: CreateUserDto, @Session() session: any) {
-  //   const user = await this.authService.signup(body.email, body.password);
-  //   session.userId = user.id;
-  //   return user;
-  // }
-
-  // @ApiResponse({type: UserDto})
-
-  // @ApiResponse({ type: UserDto })
-  // @Post('/signin')
-  // async signin(@Body() body: CreateUserDto, @Session() session: any) {
-  //   const user = await this.authService.signin(body.email, body.password);
-  //   session.userId = user.id;
-  //   return user;
-  // }
-
-
-
-  // @Get('/:id')
-  // async findUser(@Param('id') id: string) {
-  //   const user = await this.userService.findOne(id);
-  //   if (!user) {
-  //     throw new NotFoundException('user not found');
-  //   }
-  //   return user;
-  // }
-
-  // @Get()
-  // findAllUsers(@Query('login_42') login_42: string) {
-  //   return this.userService.find(login_42);
-  // }
-
-  // @Patch('/:id')
-  // updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
-  //   return this.userService.update(parseInt(id), body);
-  // }
+  @Delete('/block')
+  @ApiOperation({
+    summary: 'Remove one blocked account to the currently logger user',
+  })
+  async removeBlock(@Body() friendId: AddFriendDto, @Session() session) {
+    return await this.friendsService.removeRelation(
+      session.userId.id,
+      friendId.id,
+      RelationType.Block,
+    );
+  }
 }
