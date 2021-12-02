b511eeb3d2a16a71160afdc58d22829bdff3aabd
bvalette <bvalette@student.42.fr>
2021-12-02 11:42:58 +0100

    ♻️ refactor(back/usersdto): privateUserDto now extends UserDto

--- back_end/src/module-users/dtos/private-user.dto.ts
+++ back_end/src/module-users/dtos/private-user.dto.ts
@@ -6,30 +6,7 @@ import { UserDto } from "./user.dto";
 // TODO make privateUserDto expend UserDto to avoid repeating code
 
 @Exclude()
-export class privateUserDto {
-
-	@ApiProperty()
-	@Expose()
-	id: string;
-
-	@ApiProperty()
-	@Expose()
-	login: string;
-
-	@ApiProperty()
-	@Expose()
-	login_42: string;
-
-	@ApiProperty()
-	@Expose()
-	@Transform(value => {
-		console.log('TRANSFORM DTO', value.obj);
-		if (value.obj.use_local_photo === false || value.obj.photo_url_local === null) {
-			return value.obj.photo_url_42;
-		}
-		return value.obj.photo_url_local.fileName;
-	})
-	photo_url: string;
+export class privateUserDto extends UserDto {
 
 	@ApiProperty({ type: UserDto, isArray: true })
 	@Expose()
