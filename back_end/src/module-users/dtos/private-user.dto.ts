b88ff2be1e3f4ce3ee753b2b5a3afd0fa4c63fc7
bvalette <bvalette@student.42.fr>
2021-12-06 14:52:46 +0100

    ✨ feat(back/photo): add storeCustomePhoto info in privateUserDto

--- back_end/src/module-users/dtos/private-user.dto.ts
+++ back_end/src/module-users/dtos/private-user.dto.ts
@@ -1,34 +1,38 @@
-
-import { ApiProperty } from "@nestjs/swagger";
-import { Exclude, Expose, plainToClass, Transform } from "class-transformer";
-import { UserDto } from "./user.dto";
+import { ApiProperty } from '@nestjs/swagger';
+import { Exclude, Expose, plainToClass, Transform } from 'class-transformer';
+import { UserDto } from './user.dto';
 
 @Exclude()
 export class privateUserDto extends UserDto {
+  @ApiProperty()
+  @Expose()
+  @Transform((value) => {
+    return value.obj.photo_url_local ? true : false;
+  })
+  storeCustomPhoto: boolean;
 
-	@ApiProperty({ type: UserDto, isArray: true })
-	@Expose()
-	@Transform(value => {
-		return plainToClass(UserDto, value.obj.friends_list);
-	})
-	friends_list: UserDto[];
+  @ApiProperty({ type: UserDto, isArray: true })
+  @Expose()
+  @Transform((value) => {
+    return plainToClass(UserDto, value.obj.friends_list);
+  })
+  friends_list: UserDto[];
 
-	@ApiProperty({ type: UserDto, isArray: true })
-	@Expose()
-	@Transform(value => {
-		return plainToClass(UserDto, value.obj.blocked_list);
-	})
-	blocked_list: UserDto[];
+  @ApiProperty({ type: UserDto, isArray: true })
+  @Expose()
+  @Transform((value) => {
+    return plainToClass(UserDto, value.obj.blocked_list);
+  })
+  blocked_list: UserDto[];
 
-	@ApiProperty()
-	@Expose()
-	@Transform(value => {
-		return (value.obj.twoFASecret) ? true : false;
-	})
-  hasTwoFASecret: boolean
+  @ApiProperty()
+  @Expose()
+  @Transform((value) => {
+    return value.obj.twoFASecret ? true : false;
+  })
+  hasTwoFASecret: boolean;
 
-	// TODO remove debug
-	@Expose()
-  useTwoFA: boolean
-
+  // TODO remove debug
+  @Expose()
+  useTwoFA: boolean;
 }
