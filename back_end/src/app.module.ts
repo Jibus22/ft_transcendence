ffe96e72479def8ecfdca112840c4b601e6112f5
bvalette <bvalette@student.42.fr>
2021-12-20 10:16:13 +0100

    🚧 wip(back/chat): setup room entity, controller and service

--- back_end/src/app.module.ts
+++ back_end/src/app.module.ts
@@ -1,9 +1,16 @@
-import { CacheModule, MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { ChatModule } from './module-chat/chat.module';
+import {
+  CacheModule,
+  MiddlewareConsumer,
+  Module,
+  ValidationPipe,
+} from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Room } from './module-chat/entities/room.entity';
 import { DevelopmentModule } from './module-development/development.module';
 import { User } from './module-users/entities/users.entity';
 import { UserPhoto } from './module-users/entities/users_photo.entity';
@@ -13,6 +20,7 @@ const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
+    ChatModule,
     CacheModule.register({
       isGlobal: true,
     }),
@@ -22,7 +30,7 @@ const cookieSession = require('cookie-session');
     TypeOrmModule.forRoot({
       type: 'better-sqlite3',
       database: process.env.DB_NAME,
-      entities: [User, UserPhoto],
+      entities: [User, UserPhoto, Room],
       synchronize: true,
     }),
     UsersModule,
