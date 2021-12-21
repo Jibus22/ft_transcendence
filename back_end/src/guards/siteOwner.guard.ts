92dbb203cca64c1142b86e5731116579b0fcef21
bvalette <bvalette@student.42.fr>
2021-12-21 10:47:14 +0100

    ✨ feat(back/auth): add siteOwner guard, temporately deactivated

--- back_end/src/guards/siteOwner.guard.ts
+++ back_end/src/guards/siteOwner.guard.ts
@@ -1,18 +1,16 @@
 import { CanActivate, ExecutionContext, Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
-
-  private isTwoFaOk(session): boolean {
-    return ( ! session.useTwoFA
-      || (session.useTwoFA && session.isTwoFAutanticated));
-  }
+export class SiteOwnerGuard implements CanActivate {
 
   canActivate(context: ExecutionContext) {
 
-		const logger = new Logger( '💂‍♂️ AuthGuard'); //TODO REMOVE LOGGER HERE
+		const logger = new Logger( '💂‍♂️ Site Owner Guard'); //TODO REMOVE LOGGER HERE
     const session = context.switchToHttp().getRequest().session;
-    if (session && session.userId && this.isTwoFaOk(session)) {
+
+    return true; // TODO REMOVE !!!!!! ---- FOR TEST ONLY
+
+    if (session && session.userId && session.isSiteOwner) {
       logger.log(`User id: ${session.userId}`);
       return true;
     }
