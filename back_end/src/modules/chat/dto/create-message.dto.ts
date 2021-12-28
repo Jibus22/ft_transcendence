e4b82ef28bdec0c6053277c78f737f30086b26df
bvalette <bvalette@student.42.fr>
2021-12-28 06:39:03 +0100

    👓 chore(back/format): organize imports + format all files in back/src

--- back_end/src/modules/chat/dto/create-message.dto.ts
+++ back_end/src/modules/chat/dto/create-message.dto.ts
@@ -1,9 +1,5 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import {
-  IsOptional,
-  IsString,
-  Length
-} from 'class-validator';
+import { IsOptional, IsString, Length } from 'class-validator';
 
 export class createMessageDto {
   @ApiProperty()
