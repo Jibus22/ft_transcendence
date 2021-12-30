f8e8b828544d456dc2eefc871a3c4f647ee7430e
bvalette <bvalette@student.42.fr>
2021-12-30 10:52:17 +0100

    🐛 fix(back/guards): fix types in room guards

--- back_end/src/guards/roomModerator.guard.ts
+++ back_end/src/guards/roomModerator.guard.ts
@@ -9,18 +9,18 @@ import { Room } from '../modules/chat/entities/room.entity';
 import { User } from '../modules/users/entities/users.entity';
 
 @Injectable()
-export class RoomOwnerGuard implements CanActivate {
+export class RoomModeratorGuard implements CanActivate {
   private isRoomOwned(currentUser: User, targetedRoom: Room): boolean {
     return targetedRoom.participants.some(
       (participant) =>
-        participant.user === currentUser && participant.is_moderator,
+        participant.user.id === currentUser.id && participant.is_moderator,
     );
   }
 
   canActivate(context: ExecutionContext) {
     const logger = new Logger('💂‍♂️ Room Owner Guard'); //TODO REMOVE LOGGER HERE
     const currentUser: User = context.switchToHttp().getRequest().currentUser;
-    const targetRoom = context.switchToHttp().getRequest().params.room_id;
+    const targetRoom: Room = context.switchToHttp().getRequest().targetedRoom;
     if (currentUser && targetRoom) {
       logger.log(
         `User id: ${currentUser.id}, trying to target room: ${targetRoom}`,
