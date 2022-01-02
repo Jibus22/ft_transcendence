de0936d1748b75902ac4af781550e6123967387b
violettagoldman <violagoldman@mail.ru>
2022-01-02 20:21:20 +0100

    feat(front/chat): merge + display chat

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import * as React from "react";
 import { Homepage, MainPage, Chat } from './components';
-=======
-import * as React from 'react';
-import { Homepage, MainPage } from './components';
->>>>>>> dev
 // import ErrorPage from "./components/errorPage/ErrorPage";
 import { Routes, Route } from 'react-router-dom';
 
@@ -12,37 +7,17 @@ import { MainPageProvider } from './MainPageContext';
 import './app.css';
 
 const App = () => {
-<<<<<<< HEAD
-  return (
-    
-      <div className='home App'>
-        
-
-        
-        <Routes> 
-          {/* <Route path='*' element={<ErrorPage />} /> */}
-           <Route path='/' element={ <Homepage/> }/>
-           <Route path='/*' element={ <MainPage/> }/>
-       </Routes>
-
-       <Chat />
-    
-      </div>
-  )
-}
-=======
 	return (
 		<MainPageProvider>
 			<div className="home App">
 				<Routes>
 					{/* <Route path='*' element={<ErrorPage />} /> */}
 					<Route path="/" element={<Homepage />} />
-					<Route path="/*" element={<MainPage />} />
+					<Route path="/*" element={(<><MainPage /><Chat /></>)} />
 				</Routes>
 			</div>
 		</MainPageProvider>
 	);
 };
->>>>>>> dev
 
 export default App;
--- front_end/src/components/index.tsx
+++ front_end/src/components/index.tsx
@@ -2,32 +2,6 @@ export { default as Homepage } from './homePage/HomePage';
 export { default as TitlePage } from './homePage/mainTitle/MainTitlePage';
 export { default as Section } from './homePage/section/Section';
 export { default as Loggin } from './homePage/loggin/Loggin';
-<<<<<<< HEAD
-export { default as Footer } from './homePage/footer/Footer';
-
-export {default as MainPage} from './mainPage/MainPage'
-
-export {default as Header} from './mainPage/header/Header'
-
-
-export {default as Chat} from './chat/Chat.component'
-export {default as ChatRoom} from './mainPage/chatroom/ChatRoom'
-export {default as WindowChat} from './mainPage/chatroom/windows/WindowChat'
-
-export {default as Play} from './mainPage/game/Play/Play'
-
-export {default as ParamUser} from './mainPage/paramUser/ParamUser'
-// export {default as HistoryGame} from './mainPage/paramUser/histGame/HistoryGame'
-export {default as HistoryGame} from './mainPage/historyGame/HistoryGame'
-
-export {default as UserRank} from './mainPage/rank/UserRank'
-export {default as RankFriends} from './mainPage/rank/friends/RankFriends'
-export {default as RankWorld} from './mainPage/rank/world/RankWorld'
-
-export {default as OnlineGame} from  './mainPage/game/onlineGame/OnlineGame'
-export {default as Game} from  './mainPage/game/Game'
-// export {default as ErrorPage} from './errorPage/ErrorPage'
-=======
 export { default as MainPage } from './mainPage/MainPage';
 export { default as Header } from './mainPage/header/Header';
 export { default as ChatRoom } from './mainPage/chatroom/ChatRoom';
@@ -42,4 +16,4 @@ export { default as OnlineGame } from './mainPage/game/onlineGame/OnlineGame';
 export { default as Game } from './mainPage/game/Game';
 export { default as SnackBarre } from './mainPage/snackBar/SnackBarre';
 export { default as ErrorPage } from './mainPage/errorPage/ErrorPage';
->>>>>>> dev
+export { default as Chat } from './chat/Chat.component';
