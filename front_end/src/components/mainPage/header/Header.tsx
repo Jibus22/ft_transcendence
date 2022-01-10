efaf236e6e0b216ee5c067f52935cf3d474a2f4e
Francois Franceschi <frfrance@student.42.fr>
2022-01-10 20:39:18 +0100

    responsive maj

--- front_end/src/components/mainPage/header/Header.tsx
+++ front_end/src/components/mainPage/header/Header.tsx
@@ -2,11 +2,13 @@ import React, { useEffect } from 'react';
 import './header.scss';
 import { useMainPage } from '../../../MainPageContext';
 import { NavLink as Link } from 'react-router-dom';
-import { Badge, Avatar } from '@mui/material';
+import { Badge, Avatar, useMediaQuery } from '@mui/material';
 // import { withStyles } from '@material-ui/core/styles';
 
 export default function Header() {
 	const { data, userName, userImg, setUserName, setUserImg } = useMainPage();
+
+	const query = useMediaQuery('(max-width:850px)');
 
 	useEffect(() => {
 		if (data.length > 0) {
@@ -18,7 +20,7 @@ export default function Header() {
 
 	return (
 		<div className=" d-flex flex-column mainHeader ">
-			<nav className="navbar navbar-expand-lg  menuHeader stroke ">
+			<nav className="navbar   menuHeader stroke ">
 				<div className="d-flex  mainNavMenu">
 					<ul className="navbar-nav headerMenu  w-100 ">
 						<li className="nav-item  linkLogoNav">
@@ -42,31 +44,33 @@ export default function Header() {
 				</div>
 
 				<div className="logHeader d-flex ">
-					<Link className={(navData) => (navData.isActive ? 'active' : '')} to="/Setting">
+					<Link className={(navData) => (navData.isActive && query ? 'selectedNave' : '')} to="/Setting">
 						<div className="profil d-flex   ">
 							<div className="profilLoggin  ">
 								<h2 className="">{userName}</h2>
 							</div>
-							<div className="profilLogginImg ">
-								<Badge
-									overlap="circular"
-									anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-									variant="dot"
-									sx={{
-										'.MuiBadge-badge': {
-											backgroundColor: 'green',
-											color: 'green',
-											borderColor: 'green',
-											boxShadow: 'green',
-										},
-										'.MuiBadge-badge::after': {
-											borderColor: 'green',
-										},
-									}}
-								>
-									<Avatar alt="userImg" src={userImg} />
-								</Badge>
-							</div>
+							{!query ? (
+								<div className="profilLogginImg ">
+									<Badge
+										overlap="circular"
+										anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+										variant="dot"
+										sx={{
+											'.MuiBadge-badge': {
+												backgroundColor: 'green',
+												color: 'green',
+												borderColor: 'green',
+												boxShadow: 'green',
+											},
+											'.MuiBadge-badge::after': {
+												borderColor: 'green',
+											},
+										}}
+									>
+										<Avatar alt="userImg" src={userImg} />
+									</Badge>
+								</div>
+							) : null}
 						</div>
 					</Link>
 				</div>
