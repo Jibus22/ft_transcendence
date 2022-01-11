12544c05894d4a9405f00ed3d87bb3911ec5eacf
bvalette <bvalette@student.42.fr>
2022-01-11 11:15:10 +0100

    ♻️ refactor(clientWs): WIP refractor mainPage ws client logic

--- front_end/src/components/mainPage/MainPage.tsx
+++ front_end/src/components/mainPage/MainPage.tsx
@@ -31,56 +31,147 @@ const MainPage = () => {
 		}
 	};
 
-	const connectWsStatus = async () => {
-		try {
-			const response = await axios('http://localhost:3000/auth/ws/token', {
-				withCredentials: true,
-			});
+	const setWsCallbacks = (socket: Socket) => {
+		socket.on('connect', () => {
+			setWsStatus(socket);
+			console.log(`WS CONNECTED`);
+		});
+
+		socket.on('disconnect', () => {
+			setWsStatus(undefined);
+			console.log(`WS DISCONNECTED`);
+		});
+
+		socket.io.on("reconnect_attempt", async () => {
+			console.log('🛑 RECONNECT ATTEMPT');
+			setTimeout(async () => {
+				await doConnectWs();
+			}, (500));
+		});
+
+		socket.io.on("error", async (error) => {
+			console.log('🛑  ERROR', error);
+			setTimeout(async () => {
+				await doConnectWs();
+			}, (500));
+		});
+
+		// socket.io.on('error', (error) => {
+		// 	console.log('RECEIVED ERROR', error);
+		// });
+
+		socket.on('publicRoomCreated', (message) => {
+			console.log('✅  publicRoomCreated', message);
+		});
+		socket.on('publicRoomRemoved', (message) => {
+			console.log('🚮  publicRoomRemoved', message);
+		});
+
+		socket.on('newMessage', (message) => {
+			console.log(`💌  Event: newMessage ->`, message);
+		});
+
+		socket.on('participantJoined', (message) => {
+			console.log(`💌  Event: participantJoined ->`, message);
+		});
+
+		socket.on('participantLeft', (message) => {
+			console.log(`💌  Event: participantLeft ->`, message);
+		});
+
+		socket.on('userAdded', (message) => {
+			console.log(`💌  Event: userAdded ->`, message);
+		});
+
+		socket.on('userBanned', (message) => {
+			console.log(`💌  Event: userBanned ->`, message);
+		});
+
+		socket.on('userMuted', (message) => {
+			console.log(`💌  Event: userMuted ->`, message);
+		});
+
+		socket.on('userModeration', (message) => {
+			console.log(`💌  Event: userModeration ->`, message);
+		});
+
+		socket.on('connect_error', async (err) => {
+			console.log('connect_error', err);
+		});
+		// socket.on('connect_error', async (err) => {
+		// 	setWsStatus(undefined);
+		// 	console.log(`ws connect_error due to ${err.message}: trying again...`);
+		// 	await doConnectWs()
+		// 		.then(async (socket) => {
+		// 			if (socket.connected) {
+		// 				console.log(`WS connected after retry.`, socket);
+		// 				setWsStatus(socket);
+		// 			} else {
+		// 				console.log(`WS seems not connecter, retrying...`);
+		// 				return await doConnectWs();
+		// 			}
+		// 		})
+		// 		.then((socket) => {
+		// 			console.log(`WS connected after retry.`, socket);
+		// 			setWsStatus(socket);
+		// 		})
+		// 		.catch((err) => {
+		// 			console.log(`WS failed to connect after retry: ${err}`);
+		// 		});
+		// });
+	};
+
+	// const tryReconnect = (socket: Socket) => {
+	// 	console.log('try reconnect...........');
+	// 	setTimeout(() => {
+	// 		socket.io.open((err) => {
+	// 			if (err) {
+	// 				tryReconnect(socket);
+	// 			}
+	// 		});
+	// 	}, 2000);
+	// }
+
+	const doConnectWs = async () => {
+		return await axios('http://localhost:3000/auth/ws/token', {
+			withCredentials: true,
+		}).then((response) => {
 			const { token } = response.data;
 			if (!token) {
 				throw new Error('no valid token');
 			}
-			const socket = io('ws://localhost:3000/chat', {
+			return io('ws://localhost:3000/chat', {
 				auth: {
 					key: token,
 				},
 			});
-			socket.on('connect_error', (err) => {
-				setWsStatus(undefined);
-				console.log(`ws connect_error due to ${err.message}`);
-			});
+		});
+	};
 
-			socket.on('connect', () => {
-				setWsStatus(socket);
-				console.log(`WS CONNECTED`);
-			});
-
-			socket.on('error', (error) => {
-				console.log('RECEIVED ERROR', error);
-			});
-
-			socket.on('disconnect', () => {
-				setWsStatus(undefined);
-				console.log(`WS DISCONNECTED`);
-			});
-
-			socket.on('publicRoomCreated', (message) => {
-				console.log('✅  publicRoomCreated', message)
-			});
-			socket.on('publicRoomRemoved', (message) => {
-				console.log('🚮  publicRoomRemoved', message)
-			});
-
-			socket.on('newMessage', (message) => {
-				console.log(`✅  NEW MESSAGE in room: message.room_id`, message)
-			});
+	const connectWsStatus = async () => {
+		try {
+			await doConnectWs()
+				.then((socket) => socket)
+				.catch(async (err) => {
+					console.log(`ws connection process failed due to: ${err}. [trying again...]`);
+					return await doConnectWs();
+				})
+				.then((socket) => {
+					setWsCallbacks(socket);
+				})
+				.catch((err) => {
+					throw Error(err);
+				});
 
 		} catch (error) {
 			const err = error as AxiosError;
+			console.log(`ws connection process failed due to: ${err.message}`);
+			setWsStatus(undefined);
 			if (err.response?.status === 401) {
+				// -> TODO : je pense que ds tous les cas il faut faire une redirection:
+				//la ws va servir au jeu et au chat donc sans elle, rien ne marche
 				navigate('/');
 			}
-			setWsStatus(undefined);
 		}
 	};
 
