5facf4edbf97d5a5eae268eca234d25b010f32a1
violettagoldman <violagoldman@mail.ru>
2022-01-11 20:11:48 +0100

    feat(front/chat): list public chats

--- front_end/src/components/chat/ChatList.component.tsx
+++ front_end/src/components/chat/ChatList.component.tsx
@@ -1,8 +1,10 @@
 import styled from "styled-components"
 import SearchIcon from '@mui/icons-material/Search';
-import { useState } from "react";
 import ChatIcon from '@mui/icons-material/Chat';
 import PeopleIcon from '@mui/icons-material/People';
+import PersonIcon from '@mui/icons-material/Person';
+import axios from "axios";
+import { useEffect, useState }  from 'react'
 
 const contacts = [
 	{ id: 0, name: "Marie", lastMessage: "Hey!", profilePicture: "https://i.pravatar.cc/200" },
@@ -13,9 +15,28 @@ const contacts = [
 	{ id: 5, name: "Jean-Marc", lastMessage: "Hey!", profilePicture: "https://i.pravatar.cc/200?e" },
 ];
 
+const publicChatName = (participants: any) => {
+	let name = "";
+	participants.forEach((p: any) => name += p.user.login[0]);
+	return name;
+}
+
 const ChatList = () => {
 
 	const [tab, setTab] = useState(0);
+	const [publicChats, setPublicChats] = useState([]);
+
+	const getChats = async () => {
+		const { data } = await axios.get("http://localhost:3000/room/publics", {
+			withCredentials: true
+		});
+		console.log(data);
+		setPublicChats(data);
+	};
+
+	useEffect(() => {
+		getChats();
+	}, []);
 
 	return (
 	<ChatListWrapper>
@@ -23,7 +44,7 @@ const ChatList = () => {
 			<input type="text" placeholder="Search" />
 			<SearchIcon style={{ fontSize: "32px", color: "#CA6C88" }} className="icon" />
 		</SearchField>
-		<List>
+		{ tab === 0 && (<List>
 			{contacts.map(contact => (<Preview key={contact.name}>
 				<img src={contact.profilePicture} alt={contact.name} />
 				<div>
@@ -31,12 +52,23 @@ const ChatList = () => {
 					<p>{contact.lastMessage}</p>
 				</div>
 			</Preview>))}
-		</List>
+		</List>)}
+		{ tab === 2 && (<List>
+			{publicChats.map((chat: any) => (<Preview key={chat.id}>
+				<div>
+					<h4>{publicChatName(chat.participants)}</h4>
+					<p>{chat.id}</p>
+				</div>
+			</Preview>))}
+		</List>)}
 		<Tabs>
 			<Tab selected={tab === 0} onClick={() => setTab(0)}>
 				<ChatIcon />
 			</Tab>
 			<Tab selected={tab === 1} onClick={() => setTab(1)}>
+				<PersonIcon />
+			</Tab>
+			<Tab selected={tab === 2} onClick={() => setTab(2)}>
 				<PeopleIcon />
 			</Tab>
 		</Tabs>
