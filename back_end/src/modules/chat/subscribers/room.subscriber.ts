dddfb9a4b7ccde726dc98e9eef170a0ed92cc40b
bvalette <bvalette@student.42.fr>
2022-01-15 08:01:05 +0100

    ✨ feat(Events): implement Events constants for events names

--- back_end/src/modules/chat/subscribers/room.subscriber.ts
+++ back_end/src/modules/chat/subscribers/room.subscriber.ts
@@ -9,6 +9,7 @@ import {
 import { RoomDto } from '../dto/room.dto';
 import { Room } from '../entities/room.entity';
 import { ChatGateway } from '../gateways/chat.gateway';
+import { Events } from '../gateways/chat.gateway';
 
 @EventSubscriber()
 export class RoomSubscriber implements EntitySubscriberInterface<Room> {
@@ -26,18 +27,21 @@ export class RoomSubscriber implements EntitySubscriberInterface<Room> {
   afterInsert(event: InsertEvent<Room>) {
     if (event.entity.is_private === false) {
       this.chatGateway.sendEventToServer(
-        'publicRoomCreated',
+        Events.PUBLIC_ROOM_CREATED,
         plainToClass(RoomDto, event.entity, { excludeExtraneousValues: true }),
-      );
+        );
+      }
     }
-  }
 
-  afterRemove(event: RemoveEvent<Room>) {
-    if (event.entity.is_private === false) {
-      delete event.entity?.participants;
-      delete event.entity?.restrictions;
-      this.chatGateway.sendEventToServer(
-        'publicRoomRemoved',
+    beforeRemove(event: RemoveEvent<Room>) {
+      console.log('REMOVE ROOM', event.entity);
+
+      if (event.entity.is_private === false) {
+        console.log('REMOVE ROOM + EMIT EVENT');
+        delete event.entity?.participants;
+        delete event.entity?.restrictions;
+        this.chatGateway.sendEventToServer(
+        Events.PUBLIC_ROOM_REMOVED,
         plainToClass(RoomDto, event.entity, { excludeExtraneousValues: true }),
       );
     }
