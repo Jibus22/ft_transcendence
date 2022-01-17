ff0ae7d3434a3cbb8c5788032147b1c1bdbe6a5b
Francois Franceschi <frfrance@student.42.fr>
2022-01-17 15:31:06 +0100

    history data done

--- front_end/src/components/mainPage/historyGame/formHistory/FormHistory.tsx
+++ front_end/src/components/mainPage/historyGame/formHistory/FormHistory.tsx
@@ -1,67 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import {
-	InputAdornment,
-	Button,
-	FormControl,
-	FormLabel,
-	IconButton,
-	AvatarGroup,
-	Avatar,
-	Badge,
-	TextField,
-	useMediaQuery,
-	Box,
-} from '@mui/material';
+import React from 'react';
+import { InputAdornment, TextField } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-import axios, { AxiosError } from 'axios';
 import './formHistory.scss';
-import { makeStyles, withStyles } from '@mui/styles';
 
-// const StyledTextField = withStyles({
-// 	root: {
-// 		'& label': {
-// 			width: '100%',
-// 			border: '1px solid black',
-// 			// textAlign: 'left',
-// 			// transformOrigin: 'left',
-// 			'&.Mui-focused': {
-// 				// transformOrigin: 'center',
-// 			},
-// 		},
-// 	},
-// })(TextField);
+interface Props {
+	name: string;
+	filter: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	isActive: boolean;
+}
 
-export default function FormHistory() {
+export default function FormHistory({ name, filter, isActive }: Props) {
 	return (
 		<div className="mainFormHistory children">
-			<form className="w-100 h-100 formMainFormHistory d-flex">
-				<TextField
-					sx={{ width: 2 / 2 }}
-					name="nickname"
-					id="outlined-basic"
-					variant="outlined"
-					label="Search"
-					autoComplete="off"
-					// size={'small'}
-					// rows={1.2}
-					// placeholder={userName}
-					// value={formik.values.nickname}
-					// onChange={formik.handleChange}
-					// error={formik.touched.nickname && Boolean(formik.errors.nickname)}
-					// helperText={formik && formik.errors.nickname}
-					// disabled={disableInputPop()}
-
-					InputProps={{
-						endAdornment: (
-							<InputAdornment position="end">
-								<IconButton>
-									<SearchIcon sx={{ color: 'white', width: 2 / 2, height: 2 / 2 }} />
-								</IconButton>
-							</InputAdornment>
-						),
-					}}
-				/>
-			</form>
+			<TextField
+				sx={{ width: 2 / 2 }}
+				name="nickname"
+				id="outlined-basic"
+				variant="outlined"
+				label="Search"
+				autoComplete="off"
+				value={name}
+				onChange={filter}
+				disabled={isActive}
+				InputProps={{
+					endAdornment: (
+						<InputAdornment position="end">
+							<SearchIcon sx={{ color: 'white' }} />
+						</InputAdornment>
+					),
+				}}
+			/>
 		</div>
 	);
 }
