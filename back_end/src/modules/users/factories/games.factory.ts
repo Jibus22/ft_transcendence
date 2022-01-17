7772807477621e0328000929b349b94aedc524d5
bvalette <bvalette@student.42.fr>
2022-01-17 18:32:51 +0100

    ♻️ refactor(seed): cast timestamps as numbers in factories

--- back_end/src/modules/users/factories/games.factory.ts
+++ back_end/src/modules/users/factories/games.factory.ts
@@ -5,8 +5,9 @@ var faker = require('faker');
 
 define(Game, () => {
   const game = new Game();
-  game.updatedAt =
-    faker.time.recent() - Math.floor(Math.random() * 1000 * 60 * 60 * 24 * 365);
-  game.createdAt = game.updatedAt - 1000 * 60 * Math.round(Math.random() * 10);
+  const randomOffset = Math.floor(Math.random() * 1000 * 60 * 60 * 24 * 365);
+  game.updatedAt = (faker.time.recent() as number) - randomOffset;
+  game.createdAt =
+    game.updatedAt - 1000 * 60 * Math.floor(Math.random() * 10) + 2;
   return game;
 });
