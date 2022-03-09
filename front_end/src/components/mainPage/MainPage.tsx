ef5e50e83da039a197940a0da0afc2b5bcd09845
Le Corre <jibeguitare@hotmail.fr>
2022-03-09 22:15:55 +0100

    ♻️ refactor(game-front): put all socket connection logic in separate files and set and use the new consistent state hook setPlayerGameLogic

--- front_end/src/components/mainPage/MainPage.tsx
+++ front_end/src/components/mainPage/MainPage.tsx
@@ -6,8 +6,10 @@ import { Route, Routes, useNavigate } from 'react-router-dom';
 import { io, Socket } from 'socket.io-client';
 import { ErrorPage, Game, Header, HistoryGame, ParamUser, SnackBarre, UserRank } from '..';
 import { useMainPage } from '../../MainPageContext';
-import { OnlineGameRemooveType, OnlineGameType, UserDto, UserMe } from '../type';
+import { OnlineGameRemooveType, OnlineGameType, PlayerGameLogic, UserDto, UserMe } from '../type';
 import './mainPage.scss';
+import { doDisconnect, connectWs } from './socketInit/socketCoreInit';
+import { gameCallbacks, setWsCallbacks } from './socketInit/socketCbInit';
 
 const MainPage = () => {
 	const {
@@ -33,26 +35,17 @@ const MainPage = () => {
 		userName,
 	} = useMainPage();
 
-	// const [chatWs, setChatWs] = useSafeState<Socket | undefined>(undefined);
-	const [chatWs, setChatWs] = useState<Socket | undefined>(undefined);
 	// const [connectionTrieschatWs, setConnectionsTriesChatWs] = useState<number>(0);
-
 	// const [gameWs, setGameWs] = useState<Socket | undefined>(undefined);
-
 	// const [connectionTriesgameWs, setConnectionsTriesGameWs] = useState<number>(0);
+	// const [load, setLoad] = useState(false);
+	const [chatWs, setChatWs] = useState<Socket | undefined>(undefined);
 	const [time, setTime] = useState(false);
 	const [isHeader, setIsHeader] = useState(true);
 	const [openDialog, setOpenDIalog] = useState(false);
-
-	const [wsId, setWsId] = useState('');
-
-	let navigate = useNavigate();
-
 	const [timeSnack, setTimeSnack] = useState(false);
 
-	const [load, setLoad] = useState(false);
-
-	let lol: string;
+	const navigate = useNavigate();
 
 	const fetchDataUserMe = async () => {
 		try {
@@ -60,11 +53,7 @@ const MainPage = () => {
 				withCredentials: true,
 			});
 			const user: UserMe = response.data;
-
 			setData([response.data]);
-
-			// console.log(response.data[0]['login']);
-
 			return user.login;
 		} catch (error) {
 			setOpenDIalog(true);
@@ -76,213 +65,13 @@ const MainPage = () => {
 		}
 	};
 
-	const setWsCallbacks = (
-		socket: Socket,
-		stateSetter: (value: React.SetStateAction<Socket | undefined>) => void,
-		login: string | undefined,
-	) => {
-		/* -----------------------
-		 ** Connection
-		 * -----------------------*/
-
-		socket.on('connect', () => {
-			console.log(`[CHAT SOCKET 🍄 ] WS CONNECT`);
-			stateSetter(socket);
-		});
-
-		socket.on('disconnect', () => {
-			console.log(`[CHAT SOCKET 🍄 ] WS DISCONNECTED`);
-			doConnect(socket, stateSetter);
-		});
-
-		socket.on('connect_error', async (err) => {
-			console.log('[CHAT SOCKET 🍄 ] connect_error', err);
-			connectWs(`ws://${process.env.REACT_APP_BASE_URL || 'localhost:3000'}/chat`, setWsCallbacks, stateSetter, login);
-		});
-
-		socket.io.on('error', (error) => {
-			console.log('[CHAT SOCKET 🍄 ] ⚠️ RECEIVED ERROR', error);
-		});
-
-		/* -----------------------
-		 ** Events
-		 * -----------------------*/
-
-		socket.on('publicRoomCreated', (message) => {
-			console.log(`💌  Event: publicRoomCreated ->`, message);
-			window.dispatchEvent(new CustomEvent('publicRoomCreated', { detail: message }));
-		});
-		socket.on('publicRoomUpdated', (message) => {
-			console.log(`💌  Event: publicRoomUpdated ->`, message);
-			window.dispatchEvent(new CustomEvent('publicRoomUpdated', { detail: message }));
-		});
-		socket.on('publicRoomRemoved', (message) => {
-			console.log(`💌  Event: publicRoomRemoved ->`, message);
-		});
-		socket.on('newMessage', (message) => {
-			console.log(`💌  Event: newMessage ->`, message);
-			window.dispatchEvent(new CustomEvent('newMessage', { detail: message }));
-		});
-		socket.on('roomParticipantUpdated', (message) => {
-			console.log(`💌  Event: roomParticipantUpdated ->`, message);
-			window.dispatchEvent(new CustomEvent('roomParticipantUpdated', { detail: message }));
-		});
-		socket.on('userAdded', (message) => {
-			console.log(`💌  Event: userAdded ->`, message);
-			window.dispatchEvent(new CustomEvent('userAdded', { detail: message }));
-		});
-		socket.on('userRemoved', (message) => {
-			console.log(`💌  Event: userRemoved ->`, message);
-		});
-		socket.on('userModeration', (message) => {
-			console.log(`💌  Event: userModeration ->`, message);
-			window.dispatchEvent(new CustomEvent('publicRoomUpdated', { detail: message }));
-		});
-		socket.on('userBanned', (message) => {
-			console.log(`💌  Event: userBanned ->`, message);
-		});
-		socket.on('publicUserInfosUpdated', (message) => {
-			console.log(`💌  Event: publicUserInfosUpdated ->`, message);
-			window.dispatchEvent(new CustomEvent('publicRoomUpdated', { detail: message }));
-		});
-	};
-
-	const gameCallbacks = (
-		socket: Socket,
-		stateSetter: (value: React.SetStateAction<Socket | undefined>) => void,
-		login: string | undefined,
-	) => {
-		/* -----------------------
-		 ** Connection
-		 * -----------------------*/
-
-		socket.on('connect', () => {
-			console.log(`[GAME SOCKET 🎲 ] WS CONNECT`);
-			setLoadingSocket(true);
-		});
-
-		socket.on('disconnect', () => {
-			console.log(`[GAME SOCKET 🎲 ] WS DISCONNECTED`);
-			doConnect(socket, stateSetter);
-		});
-
-		socket.on('connect_error', async (err) => {
-			console.log('[GAME SOCKET 🎲 ] connect_error', err);
-			connectWs(`ws://${process.env.REACT_APP_BASE_URL || 'localhost:3000'}/game`, gameCallbacks, stateSetter, login);
-		});
-
-		socket.io.on('error', (error) => {
-			console.log('[GAME SOCKET 🎲 ] ⚠️ RECEIVED ERROR', error);
-		});
-
-		/* -----------------------
-		 ** Game events
-		 * -----------------------*/
-
-		socket.on('gameFinished', (room: string) => {
-			console.log(`💌  Event: gameFinished -> ${room}`);
-			//enlever l'objet onlinegame de la liste des onlinegames
-		});
-
-		socket.on('goBackInGame', (obj: OnlineGameRemooveType) => {
-			console.log(`💌  Event: goBackInGame ->`);
-			// console.log('ici ====', obj);
-			//Server detected the client was playing before disconnecting so it gives
-			//thru this event an OnlineGameDto so that we can call the game component
-			//and go back to the game.
-
-			setDataUserBack(obj);
-
-			if (login === obj.challenger.login) {
-				setIsOpponant(true);
-			} else {
-				setIsOpponant(false);
-			}
-			setBackInGame(true);
-			setIsGameRandom(true);
-			setPlayerNewGameInvit(true);
-			setStartGame(true);
-		});
-
-		socket.on('myerror', (message: string) => {
-			console.log(`💌  Event: myerror -> ${message}`);
-			//catch error
-		});
-	};
-
-	/* -----------------------
-	 ** Initialization
-	 * -----------------------*/
-
-	const getAuthToken = async () => {
-		return await axios(`http://${process.env.REACT_APP_BASE_URL || 'localhost:3000'}/auth/ws/token`, {
-			withCredentials: true,
-		}).then((response) => {
-			const { token } = response.data;
-			if (!token) {
-				throw new Error('no valid token');
-			}
-			return token;
-		});
-	};
-
-	const doDisconnect = async (socket: Socket | undefined, stateSetter: (value: React.SetStateAction<Socket | undefined>) => void) => {
-		stateSetter(undefined);
-		if (socket) {
-			socket.off('disconnect');
-			socket.on('disconnect', () => {
-				console.log('user chose to leave !');
-			});
-			socket.disconnect();
-		}
-	};
-
-	const doConnect = async (socket: Socket, stateSetter: (value: React.SetStateAction<Socket | undefined>) => void) => {
-		setTimeout(async () => {
-			await getAuthToken()
-				.then((token) => {
-					console.log('DOCONNECT', socket);
-					socket.auth = { key: `${token}` };
-					socket.connect();
-					stateSetter(socket);
-				})
-				.catch((err) => {
-					console.log('DOCONNECT ERROR ->', err);
-					stateSetter(undefined);
-					doConnect(socket, stateSetter);
-				});
-		}, 1000);
-	};
-
-	const connectWs = async (
-		uri: string,
-		cbSetter: (socket: Socket, stateSetter: React.Dispatch<React.SetStateAction<Socket | undefined>>, pouet: string | undefined) => void,
-
-		stateSetter: (value: React.SetStateAction<Socket | undefined>) => void,
-		login: string | undefined,
-	) => {
-		await new Promise((res) => {
-			setTimeout(() => {
-				const socket = io(uri, {
-					autoConnect: false,
-					reconnection: false,
-					forceNew: true,
-				});
-				cbSetter(socket, stateSetter, login);
-				stateSetter(socket);
-				doConnect(socket, stateSetter);
-				res('');
-			}, 500);
-		});
-	};
-
 	useMount(async () => {
 		await fetchDataUserMe()
 			.then(async (login: string | undefined) => {
 				await connectWs(`ws://${process.env.REACT_APP_BASE_URL || 'localhost:3000'}/game`, gameCallbacks, setGameWs, login);
 				await connectWs(`ws://${process.env.REACT_APP_BASE_URL || 'localhost:3000'}/chat`, setWsCallbacks, setChatWs, login);
 			})
-			.catch((err) => {
+			.catch(() => {
 				navigate('/');
 			});
 	});
@@ -290,6 +79,7 @@ const MainPage = () => {
 	let countInvit = 0;
 	let timer: NodeJS.Timer;
 	const [progress, setProgress] = React.useState(0);
+	const [wsId, setWsId] = useState('');
 
 	const handleCloseTimeSnack = () => {
 		gameWs?.emit('gameInvitResponse', { response: 'KO', to: wsId }, (response: number) => {
@@ -313,18 +103,55 @@ const MainPage = () => {
 		clearInterval(timer);
 	};
 
-	useEffect(() => {
-		gameWs?.on('gameInvitation', async (challengerData, challengerWsId) => {
-			countInvit++;
+	// const [opponent, setOpponent] = useState(new UserDto());
+	// const [isP2, setIsP2] = useState(false);
+	// const [isJoinGame, setIsJoinGame] = useState(false);
+	const [playerGameLogic, setPlayerGameLogic] = useState(new PlayerGameLogic());
 
-			// console.log('count__invit ==', countInvit);
-			//console.log('int i  ==', i);
+	useEffect(() => {
+		/* -----------------------
+		 ** Game events
+		 * -----------------------*/
+
+		gameWs?.on('gameFinished', (room: string) => {
+			console.log(`💌  Event: gameFinished -> ${room}`);
+			//enlever l'objet onlinegame de la liste des onlinegames
+		});
+
+		gameWs?.on('goBackInGame', (obj: OnlineGameRemooveType) => {
+			console.log(`💌  Event: goBackInGame ->`);
+			// console.log('ici ====', obj);
+			//Server detected the client was playing before disconnecting so it gives
+			//thru this event an OnlineGameDto so that we can call the game component
+			//and go back to the game.
+
+			setDataUserBack(obj);
+
+			if (login === obj.challenger.login) {
+				setIsOpponant(true);
+			} else {
+				setIsOpponant(false);
+			}
+			setBackInGame(true);
+			setIsGameRandom(true);
+			setPlayerNewGameInvit(true);
+			setStartGame(true);
+		});
+
+		gameWs?.on('myerror', (message: string) => {
+			console.log(`💌  Event: myerror -> ${message}`);
+			//catch error
+		});
+
+		gameWs?.on('gameInvitation', async (challengerData: UserDto, challengerWsId: string) => {
+			countInvit++;
 			if (countInvit > 1) {
 				setDisableInvitOther(false);
 				gameWs?.emit('gameInvitResponse', { response: 'KO', to: challengerWsId });
 				countInvit--;
 			} else {
-				setChallengData([challengerData]);
+				// setChallengData([challengerData]);
+				setPlayerGameLogic({ ...playerGameLogic, isP1: false, opponent: challengerData });
 				setWsId(challengerWsId);
 				setTimeSnack(true);
 				timer = setInterval(() => {
@@ -346,9 +173,17 @@ const MainPage = () => {
 			console.log(usr);
 			setTimeSnack(false);
 		});
+
 		return () => {
 			gameWs?.off('gaveUp');
 			gameWs?.off('gameInvitation');
+			gameWs?.off('connect');
+			gameWs?.off('disconnect');
+			gameWs?.off('connect_error');
+			gameWs?.off('error');
+			gameWs?.off('gameFinished');
+			gameWs?.off('goBackInGame');
+			gameWs?.off('myerror');
 		};
 	}, [gameWs]);
 
@@ -384,10 +219,13 @@ const MainPage = () => {
 			{/* <button onClick={blabla}> push </button> */}
 
 			<Routes>
-				<Route path="/MainPage" element={<Game chatWs={chatWs} />} />
+				<Route
+					path="/MainPage"
+					element={<Game chatWs={chatWs} setPlayerGameLogic={setPlayerGameLogic} playerGameLogic={playerGameLogic} />}
+				/>
 				<Route path="/History-Game" element={<HistoryGame />} />
 				<Route path="/Setting" element={<ParamUser setTime={setTime} />} />
-				<Route path="/Rank" element={<UserRank />} />
+				<Route path="/Rank" element={<UserRank setPlayerGameLogic={setPlayerGameLogic} />} />
 				<Route path="*" element={<ErrorPage isHeader={setIsHeader} />} />
 			</Routes>
 
